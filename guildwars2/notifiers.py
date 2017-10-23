31aee7a129c8c6a80d384a7a5ea2c8578c46a8c7
Maselkov <Maselkov@users.noreply.github.com>
2017-10-23 21:49:19 +0200

    Remove duplicate definition

--- guildwars2/notifiers.py
+++ guildwars2/notifiers.py
@@ -301,61 +301,6 @@ class NotiifiersMixin:
             msg = ("Boss notifier disabled")
         await ctx.send(msg)
 
-    @commands.group()
-    @commands.guild_only()
-    @commands.has_permissions(manage_guild=True)
-    async def bossnotifier(self, ctx):
-        """Sends the next two bosses every 15 minutes to a channel
-        """
-        if ctx.invoked_subcommand is None:
-            return await self.bot.send_cmd_help(ctx)
-
-    @commands.cooldown(1, 5, BucketType.guild)
-    @bossnotifier.command(name="channel")
-    async def bossnotifier_channel(self, ctx, channel: discord.TextChannel):
-        """Sets the channel to send the bosses to"""
-        guild = ctx.guild
-        if not guild.me.permissions_in(channel).send_messages:
-            return await ctx.send("I do not have permissions to send "
-                                  "messages to {.mention}".format(channel))
-        await self.bot.database.set_guild(
-            guild, {"bossnotifs.channel": channel.id}, self)
-        doc = await self.bot.database.get_guild(guild, self)
-        enabled = doc["bossnotifs"].get("on", False)
-        if enabled:
-            msg = ("I will now send upcoming bosses to {.mention}."
-                   "\nLast message will be automatically deleted.".format(
-                       channel))
-        else:
-            msg = ("Channel set to {.mention}. In order to receive "
-                   "upcoming bosses, you still need to enable it using "
-                   "`bossnotifier toggle on`.".format(channel))
-        await channel.send(msg)
-
-    @commands.cooldown(1, 5, BucketType.guild)
-    @bossnotifier.command(name="toggle")
-    async def bossnotifier_toggle(self, ctx, on_off: bool):
-        """Toggles posting upcoming bosses"""
-        guild = ctx.guild
-        await self.bot.database.set_guild(guild, {"bossnotifs.on": on_off},
-                                          self)
-        if on_off:
-            doc = await self.bot.database.get_guild(guild, self)
-            channel = doc["bossnotifs"].get("channel")
-            if channel:
-                channel = guild.get_channel(channel)
-                if channel:
-                    msg = ("I will now send upcoming bosses to {.mention}."
-                           "\nLast message will be automatically deleted.".
-                           format(channel))
-            else:
-                msg = ("Boss notifier toggled on. In order to receive "
-                       "bosses, you still need to set a channel using "
-                       "`bossnotifier channel <channel>`.".format(channel))
-        else:
-            msg = ("Boss notifier disabled")
-        await ctx.send(msg)
-
     async def get_patchnotes(self):
         base_url = "https://en-forum.guildwars2.com"
         url_updates = base_url + "/categories/game-release-notes"
