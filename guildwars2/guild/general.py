786576138cc158a52526c91d0c9df6a34d218fa6
fabian.ihle <fabian.ihle@hfwu.de>
2017-11-13 11:14:56 +0100

    Preparing for preferred guild command

--- guildwars2/guild/general.py
+++ guildwars2/guild/general.py
@@ -179,7 +179,7 @@ class GeneralGuild:
                 guild_name = guild_name["name"]
             except APIError as e:
                 return await self.error_handler(ctx, e)
-        else:
+        elif guild_name != None:
             endpoint_id = "guild/search?name=" + guild_name.replace(' ', '%20')
             try:
                 guild_id = await self.call_api(endpoint_id)
@@ -192,6 +192,8 @@ class GeneralGuild:
                     "use this command")
             except APIError as e:
                 return await self.error_handler(ctx, e)
+        else:
+            await ctx.send("Please set either a preferred guild or set one as parameter")
 
         try:
             endpoint = "guild/{0}/log/".format(guild_id)
