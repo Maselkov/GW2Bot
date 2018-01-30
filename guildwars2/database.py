087c772d76483bc6312c5f1c6ba28890961ce542
fabian.ihle <fabian.ihle@hfwu.de>
2018-01-30 16:22:24 +0100

    Reversed changes in consolidate_duplicates

--- guildwars2/database.py
+++ guildwars2/database.py
@@ -327,13 +327,13 @@ class DatabaseMixin:
         def consolidate_duplicates(items):
             unique_items = collections.OrderedDict()
             for item in items:
-                item_tuple = item["name"], item["rarity"], item["icon"]
+                item_tuple = item["name"], item["rarity"]
                 if item_tuple not in unique_items:
                     unique_items[item_tuple] = []
                 unique_items[item_tuple].append(item["_id"])
             unique_list = []
             for k, v in unique_items.items():
-                unique_list.append({"name": k[0], "rarity": k[1], "icon": k[2], "ids": v})
+                unique_list.append({"name": k[0], "rarity": k[1], "ids": v})
             return unique_list
 
         def check(m):
