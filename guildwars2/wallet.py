1fff3f61940bb54f5c488015f8aed2613b178004
Maselkov <Maselkov@users.noreply.github.com>
2019-08-10 17:53:33 +0200

    Rework wallet to a single command

--- guildwars2/wallet.py
+++ guildwars2/wallet.py
@@ -3,24 +3,10 @@ from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 
 from .exceptions import APIError
-from .utils.db import prepare_search
-from .utils.chat import embed_list_lines, zero_width_space
+from .utils.chat import embed_list_lines
 
 
 class WalletMixin:
-    @commands.group(case_insensitive=True)
-    async def wallet(self, ctx):
-        """Wallet related commands"""
-        if ctx.invoked_subcommand is None:
-            try:
-                length = len(ctx.prefix) + len(ctx.invoked_with)
-                if length != len(ctx.message.content):
-                    arg = ctx.message.content[length + 1:]
-                    return await ctx.invoke(self.wallet_currency, currency=arg)
-            except:
-                pass
-            await ctx.send_help(ctx.command)
-
     async def get_wallet(self, ctx, ids):
         # Returns a list of lists with currency strings
 
@@ -52,8 +38,7 @@ class WalletMixin:
                 if currency["id"] in ids[i]:
                     if c_doc["name"] == "Coin":
                         lines[i].append("{} {} {}".format(
-                            emoji,
-                            self.gold_to_coins(ctx, currency["value"]),
+                            emoji, self.gold_to_coins(ctx, currency["value"]),
                             c_doc["name"]))
                     else:
                         lines[i].append("{} {} {}".format(
@@ -86,85 +71,13 @@ class WalletMixin:
                     doc = await self.db.items.find_one({"_id": k})
                     name = doc["name"]
                     emoji = self.get_emoji(ctx, name)
-                    lines[i].append("{} {} {}".format(emoji,
-                                                      sum(v.values()), name))
+                    lines[i].append("{} {} {}".format(emoji, sum(v.values()),
+                                                      name))
         return lines
 
-    @wallet.command(name="currency")
-    @commands.cooldown(1, 5, BucketType.user)
-    async def wallet_currency(self, ctx, *, currency):
-        """Info about a currency. See $wallet currencies for list"""
-        try:
-            doc = await self.fetch_key(ctx.author, ["wallet"])
-            results = await self.call_api("account/wallet", key=doc["key"])
-        except APIError as e:
-            return await self.error_handler(ctx, e)
-        currency = currency.lower()
-        if currency == "gold":
-            currency = "coin"
-        query = {"name": prepare_search(currency)}
-        count = await self.db.currencies.count_documents(query)
-        cursor = self.db.currencies.find(query)
-        choice = await self.selection_menu(ctx, cursor, count)
-        if not choice:
-            return
-        embed = discord.Embed(
-            title=choice["name"].title(),
-            description=choice["description"],
-            colour=await self.get_embed_color(ctx))
-        currency_id = choice["_id"]
-        for item in results:
-            if item["id"] == currency_id == 1:
-                count = self.gold_to_coins(ctx, item["value"])
-                break
-            elif item["id"] == currency_id:
-                count = "{:,}".format(item["value"])
-                break
-            else:
-                count = 0
-        embed.add_field(name="Amount in wallet", value=count, inline=False)
-        embed.set_thumbnail(url=choice["icon"])
-        embed.set_author(
-            name=doc["account_name"], icon_url=ctx.author.avatar_url)
-        embed.set_footer(
-            text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
-        try:
-            await ctx.send(embed=embed)
-        except discord.Forbidden:
-            await ctx.send("Need permission to embed links")
-
-    @wallet.command(name="currencies")
-    @commands.cooldown(1, 5, BucketType.user)
-    async def wallet_currencies(self, ctx):
-        """Displays a list of all available currencies"""
-        try:
-            doc = await self.fetch_key(ctx.author, ["wallet"])
-        except APIError as e:
-            return await self.error_handler(ctx, e)
-        lines = []
-        async for c in self.db.currencies.find({}):
-            name = c["name"]
-            if name == "Coin":
-                emoji = self.get_emoji(ctx, "gold")
-            else:
-                emoji = self.get_emoji(ctx, name)
-            lines.append("{} {}".format(emoji, name))
-        embed = discord.Embed(
-            description=zero_width_space,
-            colour=await self.get_embed_color(ctx))
-        embed = embed_list_lines(embed, lines, "> **CURRENCIES**", inline=True)
-        embed.set_author(
-            name=doc["account_name"], icon_url=ctx.author.avatar_url)
-        embed.set_footer(
-            text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
-        try:
-            await ctx.send(embed=embed)
-        except discord.Forbidden:
-            await ctx.send("Need permission to embed links")
-
-    @wallet.command(name="show", aliases=["keys", "maps", "tokens"])
-    @commands.cooldown(1, 5, BucketType.user)
-    async def wallet_show(self, ctx):
+    @commands.command()
+    @commands.cooldown(1, 10, BucketType.user)
+    async def wallet(self, ctx):
         """Shows your wallet
 
         Required permissions: wallet
