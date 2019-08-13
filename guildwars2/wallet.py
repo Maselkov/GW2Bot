e44768a5fcc74a5e243fdc46cb682a8a3ee84230
fabian.ihle <fabian.ihle@hfwu.de>
2019-08-13 13:44:51 +0200

    - Add crystalline ore - move unbound/volatile magic into LS3/LS4 section

--- guildwars2/wallet.py
+++ guildwars2/wallet.py
@@ -87,13 +87,14 @@ class WalletMixin:
         await ctx.trigger_typing()
         ids_cur = [1, 4, 2, 3, 18, 23, 15, 16, 50, 47]
         ids_keys = [43, 40, 41, 37, 42, 38, 44, 49]
-        ids_maps = [25, 27, 19, 22, 20, 29, 32, 34, 35, 45]
+        ids_maps = [32, 45, 25, 27, 19, 22, 20, 29, 34, 35]
+        ids_maps_items = [46682]
         ids_token = [5, 9, 11, 10, 13, 12, 14, 6, 7, 24]
         ids_raid = [28, 39]
         ids_l3 = [79280, 79469, 79899, 80332, 81127, 81706]
         ids_l4 = [86069, 86977, 87645, 88955, 89537, 90783]
         ids_wallet = [ids_cur, ids_keys, ids_maps, ids_token, ids_raid]
-        ids_items = [ids_l3, ids_l4]
+        ids_items = [ids_l3, ids_l4, ids_maps_items]
 
         currencies_wallet = await self.get_wallet(ctx, ids_wallet)
         currencies_items = await self.get_item_currency(ctx, ids_items)
@@ -107,11 +108,11 @@ class WalletMixin:
         embed = embed_list_lines(
             embed, currencies_wallet[1], "> **KEYS**", inline=True)
         embed = embed_list_lines(
-            embed, currencies_wallet[2], "> **MAP CURRENCIES**", inline=True)
+            embed, currencies_wallet[2][2:5] + currencies_items[2] + currencies_wallet[2][5:], "> **MAP CURRENCIES**", inline=True)
         embed = embed_list_lines(
-            embed, currencies_items[0], "> **LIVING SEASON 3**", inline=True)
+            embed, currencies_items[0] + [currencies_wallet[2][0]], "> **LIVING SEASON 3**", inline=True)
         embed = embed_list_lines(
-            embed, currencies_items[1], "> **LIVING SEASON 4**", inline=True)
+            embed, currencies_items[1] + [currencies_wallet[2][1]], "> **LIVING SEASON 4**", inline=True)
         embed = embed_list_lines(
             embed, currencies_wallet[4], "> **RAIDS**", inline=True)
         embed.set_author(
