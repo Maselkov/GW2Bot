84b708a802ff44bd37832c20f1a04c2dc25c74e0
Maselkov <Maselkov@users.noreply.github.com>
2021-10-12 16:10:06 +0200

    Convert pvp to slash

--- guildwars2/pvp.py
+++ guildwars2/pvp.py
@@ -1,25 +1,19 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
+from discord_slash import cog_ext
+from discord_slash.model import SlashCommandOptionType
 
 from .exceptions import APIError
 
 
 class PvpMixin:
-    @commands.group(case_insensitive=True)
-    async def pvp(self, ctx):
-        """PvP related commands.
-        """
-        if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
-
-    @pvp.command(name="stats")
-    @commands.cooldown(1, 20, BucketType.user)
+    @cog_ext.cog_subcommand(base="pvp",
+                            name="stats",
+                            base_description="PVP related commands")
     async def pvp_stats(self, ctx):
-        """Information about your general pvp stats
-
-        Required permissions: pvp
-        """
+        """Information about your general pvp stats"""
+        await ctx.defer()
         try:
             doc = await self.fetch_key(ctx.author, ["pvp"])
             results = await self.call_api("pvp/stats", key=doc["key"])
@@ -49,28 +43,71 @@ class PvpMixin:
         data.add_field(name="Rank", value=rank, inline=False)
         data.add_field(name="Total games played", value=totalgamesplayed)
         data.add_field(name="Total wins", value=totalwins)
-        data.add_field(
-            name="Total winratio", value="{}%".format(totalwinratio))
+        data.add_field(name="Total winratio",
+                       value="{}%".format(totalwinratio))
         data.add_field(name="Ranked games played", value=rankedgamesplayed)
         data.add_field(name="Ranked wins", value=rankedwins)
-        data.add_field(
-            name="Ranked winratio", value="{}%".format(rankedwinratio))
+        data.add_field(name="Ranked winratio",
+                       value="{}%".format(rankedwinratio))
         data.set_author(name=doc["account_name"])
         data.set_thumbnail(url=ranks["icon"])
-        try:
-            await ctx.send(embed=data)
-        except discord.Forbidden:
-            await ctx.send("Need permission to embed links")
+        await ctx.send(embed=data)
 
-    @pvp.command(name="professions")
-    @commands.cooldown(1, 5, BucketType.user)
-    async def pvp_professions(self, ctx, *, profession: str = None):
-        """Information about your pvp profession stats.
-        If no profession is given, defaults to general profession stats.
-        Example! $pvp professions mesmer
-
-        Required permissions: pvp
-        """
+    @cog_ext.cog_subcommand(
+        base="pvp",
+        name="professions",
+        base_description="PVP related commands",
+        options=[{
+            "name":
+            "profession",
+            "description":
+            "Profession for profession specific statistics",
+            "type":
+            SlashCommandOptionType.STRING,
+            "choices": [
+                {
+                    "value": "warrior",
+                    "name": "Warrior"
+                },
+                {
+                    "value": "guardian",
+                    "name": "Guardian"
+                },
+                {
+                    "value": "revenant",
+                    "name": "Revenant"
+                },
+                {
+                    "value": "thief",
+                    "name": "Thief"
+                },
+                {
+                    "value": "ranger",
+                    "name": "Ranger"
+                },
+                {
+                    "value": "engineer",
+                    "name": "Engineer"
+                },
+                {
+                    "value": "elementalist",
+                    "name": "Elementalist"
+                },
+                {
+                    "value": "necromancer",
+                    "name": "Necromancer"
+                },
+                {
+                    "value": "mesmer",
+                    "name": "Mesmer"
+                },
+            ],
+            "required":
+            False,
+        }])
+    async def pvp_professions(self, ctx, profession=None):
+        """Information about your pvp profession stats."""
+        await ctx.defer()
         try:
             doc = await self.fetch_key(ctx.author, ["pvp"])
             results = await self.call_api("pvp/stats", key=doc["key"])
@@ -90,71 +127,53 @@ class PvpMixin:
                         "total": total,
                         "winratio": winratio
                     }
-            mostplayed = max(
-                professionsformat, key=lambda i: professionsformat[i]['total'])
+            mostplayed = max(professionsformat,
+                             key=lambda i: professionsformat[i]['total'])
             icon = self.gamedata["professions"][mostplayed]["icon"]
             mostplayedgames = professionsformat[mostplayed]["total"]
             highestwinrate = max(
                 professionsformat,
                 key=lambda i: professionsformat[i]["winratio"])
             highestwinrategames = professionsformat[highestwinrate]["winratio"]
-            leastplayed = min(
-                professionsformat, key=lambda i: professionsformat[i]["total"])
+            leastplayed = min(professionsformat,
+                              key=lambda i: professionsformat[i]["total"])
             leastplayedgames = professionsformat[leastplayed]["total"]
             lowestestwinrate = min(
                 professionsformat,
                 key=lambda i: professionsformat[i]["winratio"])
             lowestwinrategames = professionsformat[lowestestwinrate][
                 "winratio"]
-            data = discord.Embed(
-                description="Professions",
-                color=await self.get_embed_color(ctx))
+            data = discord.Embed(description="Professions",
+                                 color=await self.get_embed_color(ctx))
             data.set_thumbnail(url=icon)
-            data.add_field(
-                name="Most played profession",
-                value="{0}, with {1}".format(mostplayed.capitalize(),
-                                             mostplayedgames))
-            data.add_field(
-                name="Highest winrate profession",
-                value="{0}, with {1}%".format(highestwinrate.capitalize(),
-                                              highestwinrategames))
-            data.add_field(
-                name="Least played profession",
-                value="{0}, with {1}".format(leastplayed.capitalize(),
-                                             leastplayedgames))
-            data.add_field(
-                name="Lowest winrate profession",
-                value="{0}, with {1}%".format(lowestestwinrate.capitalize(),
-                                              lowestwinrategames))
+            data.add_field(name="Most played profession",
+                           value="{0}, with {1}".format(
+                               mostplayed.capitalize(), mostplayedgames))
+            data.add_field(name="Highest winrate profession",
+                           value="{0}, with {1}%".format(
+                               highestwinrate.capitalize(),
+                               highestwinrategames))
+            data.add_field(name="Least played profession",
+                           value="{0}, with {1}".format(
+                               leastplayed.capitalize(), leastplayedgames))
+            data.add_field(name="Lowest winrate profession",
+                           value="{0}, with {1}%".format(
+                               lowestestwinrate.capitalize(),
+                               lowestwinrategames))
             data.set_author(name=doc["account_name"])
-            data.set_footer(
-                text="PROTIP: Use $pvp professions <profession> for "
-                "more detailed stats")
-            try:
-                await ctx.send(embed=data)
-            except discord.HTTPException:
-                await ctx.send("Need permission to embed links")
-        elif profession.lower() not in self.gamedata["professions"]:
-            await ctx.send("Invalid profession")
-        elif profession.lower() not in results["professions"]:
-            await ctx.send("You haven't played that profession!")
-        else:
-            prof = profession.lower()
-            wins = results["professions"][prof]["wins"] + \
-                results["professions"][prof]["byes"]
-            total = sum(results["professions"][prof].values())
-            winratio = int((wins / total) * 100)
-            color = self.gamedata["professions"][prof]["color"]
-            color = int(color, 0)
-            data = discord.Embed(
-                description="Stats for {0}".format(prof), colour=color)
-            data.set_thumbnail(url=self.gamedata["professions"][prof]["icon"])
-            data.add_field(
-                name="Total games played", value="{0}".format(total))
-            data.add_field(name="Wins", value="{0}".format(wins))
-            data.add_field(name="Winratio", value="{0}%".format(winratio))
-            data.set_author(name=doc["account_name"])
-            try:
-                await ctx.send(embed=data)
-            except discord.Forbidden:
-                await ctx.send("Need permission to embed links")
+            return await ctx.send(embed=data)
+        wins = (results["professions"][profession]["wins"] +
+                results["professions"][profession]["byes"])
+        total = sum(results["professions"][profession].values())
+        winratio = int((wins / total) * 100)
+        color = self.gamedata["professions"][profession]["color"]
+        color = int(color, 0)
+        data = discord.Embed(description=f"Stats for {profession}",
+                             colour=color)
+        data.set_thumbnail(
+            url=self.gamedata["professions"][profession]["icon"])
+        data.add_field(name="Total games played", value="{0}".format(total))
+        data.add_field(name="Wins", value="{0}".format(wins))
+        data.add_field(name="Winratio", value="{0}%".format(winratio))
+        data.set_author(name=doc["account_name"])
+        await ctx.send(embed=data)
