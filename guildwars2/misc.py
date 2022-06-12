1a12f60334801c9aac5cb050fbf6704fa3ef2c40
Maselkov <Maselkov@users.noreply.github.com>
2022-06-12 19:46:54 +0200

    Re-add wiki command

--- guildwars2/misc.py
+++ guildwars2/misc.py
@@ -10,73 +10,56 @@ from discord.app_commands import Choice
 
 class MiscMixin:
 
-    # Wiki has the bot's IP blacklisted, probably because the
-    # requests looked robotic.
-    # Disabled for now. Might try contacting webmaster at some point.
-    # @cog_ext.cog_slash(options=[{
-    #     "name": "search_text",
-    #     "description": "The text to search the wiki for. Example: Lion's Arch",
-    #     "type": SlashCommandOptionType.STRING,
-    #     "required": True,
-    # }, {
-    #     "name":
-    #     "language",
-    #     "description":
-    #     "The language of the wiki to search on. Optional.",
-    #     "type":
-    #     SlashCommandOptionType.STRING,
-    #     "choices": [{
-    #         "value": "en",
-    #         "name": "en"
-    #     }, {
-    #         "value": "de",
-    #         "name": "de"
-    #     }, {
-    #         "value": "fr",
-    #         "name": "fr"
-    #     }, {
-    #         "value": "es",
-    #         "name": "se"
-    #     }],
-    #     "required":
-    #     False,
-    # }])
-    # async def wiki(self, ctx, search_text, language="en"):
-    #     """Search the Guild wars 2 wiki"""
-    #     if len(search_text) > 300:
-    #         return await ctx.send("Search too long", ephemeral=True)
-    #     await interaction.response.defer()
-    #     wiki = {
-    #         "en": "https://wiki.guildwars2.com",
-    #         "de": "https://wiki-de.guildwars2.com",
-    #         "fr": "https://wiki-fr.guildwars2.com",
-    #         "es": "https://wiki-es.guildwars2.com"
-    #     }
-    #     search_url = {
-    #         "en": "{}/index.php?title=Special%3ASearch&search={}",
-    #         "de": "{}/index.php?search={}&title=Spezial%3ASuche&",
-    #         "fr": "{}/index.php?search={}&title=Spécial%3ARecherche",
-    #         "es": "{}/index.php?title=Especial%3ABuscar&search={}"
-    #     }
-    #     url = (search_url[language].format(wiki[language], search_text))
-    #     async with self.session.get(url) as r:
-    #         if r.history:  # Redirected
-    #             embed = await self.search_results_embed(ctx,
-    #                                                     "Wiki",
-    #                                                     exact_match=r)
-    #             return await ctx.send(embed=embed)
-    #         else:
-    #             results = await r.text()
-    #             soup = BeautifulSoup(results, 'html.parser')
-    #             posts = soup.find_all(
-    #                 "div", {"class": "mw-search-result-heading"})[:5]
-    #             if not posts:
-    #                 return await ctx.send("No results for your search")
-    #     embed = await self.search_results_embed(ctx,
-    #                                             "Wiki",
-    #                                             posts,
-    #                                             base_url=wiki[language])
-    #     await ctx.send(embed=embed)
+    @app_commands.command()
+    @app_commands.describe(
+        language="The language of the wiki to search on. Optional. "
+        "Defaults to English.",
+        search_text="The text to search the wiki for. Example: Lion's Arch")
+    @app_commands.choices(language=[
+        Choice(name=p.title(), value=p) for p in ["en", "fr", "es", "de"]
+    ])
+    async def wiki(
+            self,
+            interaction: discord.Interaction,
+            search_text: str,  # TODO autocomplete
+            language: str = "en"):
+        """Search the Guild wars 2 wiki"""
+        if len(search_text) > 300:
+            return await interaction.response.send_message("Search too long",
+                                                           ephemeral=True)
+        await interaction.response.defer()
+        wiki = {
+            "en": "https://wiki.guildwars2.com",
+            "de": "https://wiki-de.guildwars2.com",
+            "fr": "https://wiki-fr.guildwars2.com",
+            "es": "https://wiki-es.guildwars2.com"
+        }
+        search_url = {
+            "en": "{}/index.php?title=Special%3ASearch&search={}",
+            "de": "{}/index.php?search={}&title=Spezial%3ASuche&",
+            "fr": "{}/index.php?search={}&title=Spécial%3ARecherche",
+            "es": "{}/index.php?title=Especial%3ABuscar&search={}"
+        }
+        url = (search_url[language].format(wiki[language], search_text))
+        async with self.session.get(url) as r:
+            if r.history:  # Redirected
+                embed = await self.search_results_embed(interaction,
+                                                        "Wiki",
+                                                        exact_match=r)
+                return await interaction.followup.send(embed=embed)
+            else:
+                results = await r.text()
+                soup = BeautifulSoup(results, 'html.parser')
+                posts = soup.find_all(
+                    "div", {"class": "mw-search-result-heading"})[:5]
+                if not posts:
+                    return await interaction.followup.send(
+                        "No results for your search")
+        embed = await self.search_results_embed(interaction,
+                                                "Wiki",
+                                                posts,
+                                                base_url=wiki[language])
+        await interaction.followup.send(embed=embed)
 
     async def search_results_embed(self,
                                    ctx,
