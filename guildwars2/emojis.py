0288a50ce5a2e236206d04b537041d4453d75467
Maselkov <Maselkov@users.noreply.github.com>
2023-08-29 17:25:59 +0200

    Formatting changes

--- guildwars2/emojis.py
+++ guildwars2/emojis.py
@@ -5,24 +5,25 @@ from discord.ext import commands
 
 
 class EmojiMixin:
-
     async def prepare_emojis(self):
         doc = await self.bot.database.get_cog_config(self)
         self.emojis = doc.get("emojis", {})
 
-    def get_emoji(self,
-                  ctx,
-                  emoji,
-                  *,
-                  fallback=False,
-                  fallback_fmt="{}",
-                  return_obj=False,
-                  force_emoji=False):
+    def get_emoji(
+        self,
+        ctx,
+        emoji,
+        *,
+        fallback=False,
+        fallback_fmt="{}",
+        return_obj=False,
+        force_emoji=False
+    ):
         # This monstrosity is due for a rework. But at least it works
         def get_emoji():
             search_str = emoji.lower().replace(" ", "_")
             # Remove illegal emoji characters
-            search_str = re.sub('[\.,\,,\',\:,\;,!\?]', '', search_str)
+            search_str = re.sub("[\.,\,,',\:,\;,!\?]", "", search_str)
             emoji_id = self.emojis.get(search_str)
             if emoji_id:
                 emoji_obj = self.bot.get_emoji(emoji_id)
@@ -41,7 +42,8 @@ class EmojiMixin:
         if isinstance(ctx, discord.Webhook):
             if ctx.guild:
                 can_use = ctx.channel.permissions_for(
-                    ctx.guild.default_role).external_emojis
+                    ctx.guild.default_role
+                ).external_emojis
             else:
                 can_use = True
         elif isinstance(ctx, (discord.Message, discord.TextChannel)):
@@ -102,16 +104,22 @@ class EmojiMixin:
         for emoji in ctx.guild.emojis:
             name = emoji.name
             await self.bot.database.set_cog_config(
-                self, {"emojis." + name.lower(): emoji.id})
+                self, {"emojis." + name.lower(): emoji.id}
+            )
             registered.append(emoji.name)
         if not registered:
             return await ctx.send("No emojis were registered")
-        await ctx.send("Registered {} emojis:\n```{}```".format(
-            len(registered), ", ".join(registered)))
+        await ctx.send(
+            "Registered {} emojis:\n```{}```".format(
+                len(registered), ", ".join(registered)
+            )
+        )
         await self.prepare_emojis()
 
     def check_emoji_permission(self, interaction: discord.Interaction):
         if not interaction.guild:
             return True
+        return True
         return interaction.channel.permissions_for(
-            interaction.guild.default_role).external_emojis
+            interaction.guild.default_role
+        ).external_emojis
